2ecf500385b1957614bb41e2b2244bd3efde93fb
Zhanahmet01p <ganahmet05@mail.ru>
2024-06-10 14:15:24 +0600

    test2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,8 +1,12 @@
 function factorial(n) {
+    debugger;
     if (n === 0 || n === 1) {
         return 1;
-    } else {
+    } 
+    
+    else {
         return n * factorial(n - 1);
     }
+    
 }
 console.log(factorial(5));
