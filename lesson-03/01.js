4fee344082d7cf365a29fe36907c28fff554a20a
Zhanahmet01p <ganahmet05@mail.ru>
2024-06-22 12:53:56 +0600

    test

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,14 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"]
+
+
+function createColorString() {
+    return colors.filter(ger).join('-')
+}
+function ger(item){
+    if (item=="черный" || item=="красный" ||item=="желтый"){
+        return item;
+    }
+}
+
+console.log(createColorString());
